46b09bb3df22dd013a7592bde8d09ee584af70e0
19034609 <Upsr173511>
2020-07-16 15:59:37 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19034609/demosimplemovielist/MainActivity.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.id19034609.demosimplemovielist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvExamTips;
+    ArrayList<String> movieList;
+    ArrayAdapter<String> aaMovie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvExamTips = findViewById(R.id.listViewMovie);
+
+        movieList = new ArrayList();
+        movieList.add("Avengers Infinity War");
+        movieList.add("Justice League");
+        movieList.add("IP Man");
+        movieList.add("Train to Busan 2");
+
+        aaMovie = new ArrayAdapter(this, android.R.layout.simple_list_item_1, movieList);
+        lvExamTips.setAdapter(aaMovie);
+    }
+}
